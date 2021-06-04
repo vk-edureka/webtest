6c8952267ffdc66f3e3e8f3ecfc4fb8be9d455f9
Aravind Jettigari <aravind.7788@gmail.com>
2021-06-04 19:01:53 +0530

    capstone project

---
+++ Myseleniumproject/src/test/java/Myseleniumproject/Myseleniumproject/AppTest.java
@@ -0,0 +1,20 @@
+package Myseleniumproject.Myseleniumproject;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
